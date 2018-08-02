09049f2d82a9b8f60e22a0b6db76b201701eb451
Gabriel <mfkrgabriel@gmail.com>
2018-08-02 01:34:29 -0300

    Adding fetch module test and container component

--- source/_modules/fetchModule/fetch.js
+++ source/_modules/fetchModule/fetch.js
@@ -1,7 +1,22 @@
 'use strict';
 
+const URL = 'https://5b6264dd07412d00142acf61.mockapi.io/api/v1/cars';
+
 export default class Fetch {
   constructor(){
-    console.log('Fetch Module.');
+    this.getData(URL);
+    console.log('Fetch module ready.');
+  }
+
+  getData(URL){
+    fetch(URL)
+      .then(data => data.json())
+      .then(data => {
+
+        // var map = data.map((item) => {
+        //   console.log(item);
+        // })
+
+      });
   }
 }
--- source/_scripts/main.js
+++ source/_scripts/main.js
@@ -1,10 +1,6 @@
 import Example from '../_modules/example-module/example-module';
 import Fetch from '../_modules/fetchModule/fetch';
 
-
-const URL = 'https://5b6264dd07412d00142acf61.mockapi.io/api/v1/cars';
-
-
 'use strict';
 
 (function(){
