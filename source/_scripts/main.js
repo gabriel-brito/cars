fec89a2d179c6afc9d1ea1334ffcb3786c820542
Gabriel <mfkrgabriel@gmail.com>
2018-08-02 14:50:12 -0300

    Withdrawal module

--- source/_scripts/main.js
+++ source/_scripts/main.js
@@ -1,10 +1,8 @@
-import Example from '../_modules/example-module/example-module';
 import Fetch from '../_modules/fetchModule/fetch';
 
 'use strict';
 
 (function(){
-  let example = new Example();
   let fetch = new Fetch();
 
 })();
