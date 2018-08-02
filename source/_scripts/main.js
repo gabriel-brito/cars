3ef3aad3989282fdc7d3952255c14d28fab1e512
Gabriel <mfkrgabriel@gmail.com>
2018-08-01 23:21:06 -0300

    saving fetch URL

--- source/_scripts/main.js
+++ source/_scripts/main.js
@@ -1,8 +1,13 @@
 import Example from '../_modules/example-module/example-module';
 
+const URL = 'https://5b6264dd07412d00142acf61.mockapi.io/api/v1/cars';
+
+
 'use strict';
 
 (function(){
   let example = new Example();
+
+  console.log(URL);
 
 })();
